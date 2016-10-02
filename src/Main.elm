51d46e030b3128e0c14ce02941e37ba94b7c1d8c
Christian Stangier <christian@stangier.email>
2016-10-02 13:37:58 +0200

    add model

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,33 @@
 module Main exposing (..)
 
+import Time exposing (Time, second)
 import Html exposing (text, Html)
 
 
-main : Html msg
-main =
+-- model
+
+
+type alias Model =
+    { secondsToCountDown : Int
+    , countdownRunning : Bool
+    , atZero : Bool
+    }
+
+
+type Msg
+    = IncreaseSeconds Int
+    | TimerTick Time
+    | ToggleTimer
+    | ResetTimer
+
+
+initialModel : Model
+initialModel =
+    Model 180 False False
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( initialModel, Cmd.none )
+
     text "Hello World!"

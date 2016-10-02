c551dcd2ee6427fc33bd1ee6f74da79b08dcb2d3
Christian Stangier <christian@stangier.email>
2016-10-02 13:38:34 +0200

    add update function

--- src/Main.elm
+++ src/Main.elm
@@ -30,4 +30,36 @@ init : ( Model, Cmd Msg )
 init =
     ( initialModel, Cmd.none )
 
+
+
+-- update
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        IncreaseSeconds seconds ->
+            let
+                newSecondsToCountDown =
+                    model.secondsToCountDown + seconds
+            in
+                ( { model | secondsToCountDown = newSecondsToCountDown }, Cmd.none )
+
+        ToggleTimer ->
+            ( { model | countdownRunning = not model.countdownRunning }, Cmd.none )
+
+        TimerTick now ->
+            let
+                newSeconds =
+                    model.secondsToCountDown - 1
+
+                atZero =
+                    newSeconds == 0
+            in
+                ( { model | secondsToCountDown = newSeconds, atZero = atZero }, Cmd.none )
+
+        ResetTimer ->
+            ( initialModel, Cmd.none )
+
+
     text "Hello World!"
